b7f83376c8b799673bcefedd2cc6e337ee017c02
alifao_s <alifao_s@etna-alternance.net>
2024-10-25 18:20:54 +0000

    mise à jour

--- Base_game/getDonnées.ts
+++ Base_game/getDonnées.ts
@@ -1,67 +1,64 @@
 import { Personnage } from './interfaces';
+
 const fs = require('fs');
 
-//données liés au fichier players.json
 const players = fs.readFileSync('./jsonFiles/players.json', 'utf-8');
 const parsePlayers = JSON.parse(players);
-//données liés au fichier enemies.json
+
 const enenmies = fs.readFileSync('./jsonFiles/enemies.json', 'utf-8');
 const parseEnenmies = JSON.parse(enenmies);
-//données liés au fichier bosses.json
+
 const bosses = fs.readFileSync('./jsonFiles/bosses.json', 'utf-8');
 const parseBosses = JSON.parse(bosses);
-//genère un nombre aléatoire 
+
 const randomValue = Math.round(Math.random() * 100 + 1);
-// console.log(randomValue)
+
 let selcetedRarity : number;
- 
+
 if (randomValue > 50 && randomValue <= 100) {
- selcetedRarity  = 1
+  selcetedRarity = 1;
 } else if (randomValue > 20 && randomValue <= 50) {
- selcetedRarity  = 2
-} else if (randomValue > 5 &&  randomValue <= 20 ) {
- selcetedRarity  = 3
-} else if (randomValue > 1 &&  randomValue <= 5) {
-selcetedRarity  = 4
+  selcetedRarity = 2;
+} else if (randomValue > 5 && randomValue <= 20) {
+  selcetedRarity = 3;
+} else if (randomValue > 1 && randomValue <= 5) {
+  selcetedRarity = 4;
 } else if (randomValue === 1) {
- selcetedRarity = 5
+  selcetedRarity = 5;
 } else {
- selcetedRarity  = 0
+  selcetedRarity = 0;
 }
 
 function getPlayers() {
-    const tableau: Array<Personnage> = []; 
-    for (const elements of parsePlayers) {
-        if (selcetedRarity === elements.rarity) {
-          tableau.push(elements)
-        }
-    }  
-    const random_tab_index = Math.round(Math.random() * tableau.length - 1)
-    return tableau[random_tab_index];
+  const tableau: Array<Personnage> = [];
+  for (const elements of parsePlayers) {
+    if (selcetedRarity === elements.rarity) {
+      tableau.push(elements);
+    }
+  }
+  const randomTabIndex = Math.floor(Math.random() * tableau.length);
+  return tableau[randomTabIndex];
 }
 
 function getEnemies() {
-    const tableau: Array<Personnage> = []; 
-    for (const elements of parseEnenmies) {
-        // console.log(elements.rarity)
-        if (selcetedRarity === elements.rarity) {
-          tableau.push(elements)
-        }
+  const tableau: Array<Personnage> = [];
+  for (const elements of parseEnenmies) {
+    if (selcetedRarity === elements.rarity) {
+      tableau.push(elements);
     }
-    const random_tab_index = Math.round(Math.random() * tableau.length - 1)
-    console.log(tableau[random_tab_index])
-    return tableau[random_tab_index];
+  }
+  const randomTabIndex = Math.floor(Math.random() * tableau.length);
+  return tableau[randomTabIndex];
 }
 function getBosses() {
-    const tableau: Array<Personnage> = []; 
-    for (const elements of parseBosses) {
-        // console.log(elements.rarity)
-        if (selcetedRarity === elements.rarity) {
-         tableau.push(elements)
-        }
-    } 
-    const random_tab_index = Math.round(Math.random() * tableau.length - 1)
-    return tableau[random_tab_index]; 
+  const tableau: Array<Personnage> = [];
+  for (const elements of parseBosses) {
+    if (selcetedRarity === elements.rarity) {
+      tableau.push(elements);
+    }
+  }
+  const randomTabIndex = Math.floor(Math.random() * tableau.length);
+  return tableau[randomTabIndex];
 }
 
-export {getPlayers,getBosses, getEnemies};
+export { getPlayers, getBosses, getEnemies };
--- Base_game/hyrule_castle.ts
+++ Base_game/hyrule_castle.ts
@@ -1,103 +1,80 @@
-const fs = require('fs');
 import 'colorts/lib/string';
 import { Personnage } from './interfaces';
-import { attack,heal, affichVie } from './functions';
-import { getBosses, getEnemies} from './getDonnées';
+import { attack, heal, affichVie } from './functions';
+import { getBosses, getEnemies, getPlayers } from './getDonnées';
+
 const readline = require('readline-sync');
 
 function game(player:Personnage) {
-    let Fight = 0;
-    let floor: number = 1;
-    
-    //tant que l'étage actuelle est inferieur à 10 (nombre d'étage maximal) 
-    // Et tant que les vie du joueur sont supérieur à 0
-    while (floor <= 10 && player.hp > 0) {
-     const enemie =  getEnemies()
-     enemie.maxHp = enemie.hp
-      // tant que les vies du joueur et de l'énemie sont supérieur à 0
-      while (player.hp > 0 && enemie.hp > 0) {
-        //on affiche l'étage actuel
-        console.log();       
-        console.log(`******** Etage ${floor} ********\n`.yellow);
-        //nombre de combats 
-        console.log(`=== Fight ${Fight += 1} ===`);
-        //Affiche le nombre de vie de player et de l'enemie
-        let answer = affichVie(enemie,player)
-        //Verification de l'option que l'utilisateur aura choisie
-        //Si c'est attaque on attaque l'enemie
-        //Sinon on guérit 
-        if (answer === 'Attack') {
-          attack(player, enemie)
-        } else {
-          heal(player)
+  let Fight = 0;
+  let floor: number = 1;
+  const enemie = getEnemies();
+  enemie.maxHp = enemie.hp
+
+  while (floor <= 10 && player.hp > 0) {
+    enemie.hp = enemie.maxHp;
+    while (player.hp > 0 && enemie.hp > 0) {
+      console.log();
+      console.log(`******** Etage ${floor} ********\n`.yellow);
+      console.log(`=== Fight ${Fight += 1} ===`);
+      const answer = affichVie(enemie, player);
+      if (answer === 'Attack') {
+        attack(player, enemie);
+      } else {
+        heal(player);
+      }
+      affichVie(enemie, player);
+      if (enemie.hp > 0) {
+        console.log(`${enemie.name} attacked and dealt ${enemie.str} damage!\n`);
+        player.hp -= enemie.str;
+        if (player.hp < 0) {
+          player.hp = 0;
         }
-        //Affichage des vies car sinon on ne verra pas l'evolution de l'option heal
-        affichVie(enemie,player)
-        //Attaque de l'enemie
-        if(enemie.hp > 0) {
-          console.log(`${enemie.name} attacked and dealt ${enemie.str} damage!\n`);
-          player.hp -= enemie.str; 
+      } else {
+        console.log(`${enemie.name} Bokoblin died!\n`);
+      }
+      console.log('********************************************* ');
+    }
+    let reponse = readline.keyInYN(`Voulez vous monter à l'étage N°${floor + 1} ??`);
+    console.log('tt',reponse)
+    if (reponse) {
+      console.log("reponse bien");
+      floor += 1;
+    } else {
+      console.log("reponse pas bien");
+      console.log('Dommage, à bientot!!!!');
+      return false;
+    }
+    console.log("apres le if avec floor = ",floor);
+    if (floor === 10) {
+      const boss = getBosses();
+      boss.maxHp = boss.hp;
+      while (player.hp > 0 && boss.hp > 0) {
+        console.log(` Dans cette étage vous allez affronter ${boss.name}`);
+        const answer = affichVie(boss, player);
+        if (answer === 'Attack') {
+          attack(player, boss);
+        } else {
+          heal(player);
+        }
+        affichVie(boss, player);
+        if (boss.hp > 0) {
+          console.log(`${boss.name} attacked and dealt ${boss.str} damage!`);
+          player.hp -= boss.str;
           if (player.hp < 0) {
             player.hp = 0;
           }
-        } else {
-          console.log('Bokoblin died!\n')
-        }
-        console.log('********************************************* ')
-      }
-      //Avant de monter d'une étage on lui demande s'il souhaite continuer
-      const answer =readline.keyInYN(`Voulez vous monter à l'étage N°${floor+1} ??`);
-  
-      if (answer === true) {
-        floor++
-      } else {
-        console.log('Dommage, à bientot!!!!')
-        return false
-      }
-      //On monte d'un étage dès que l'enemie soit mort 
-      
-      //Dans le cas ou l'étage est 10 on lui precise qu'il affronte un autre énemie
-      if (floor === 10) {
-        let boss = getBosses()
-        boss.maxHp = boss.hp
-        //Tant que le nombre de vie du joueur et de l'énemie sont supérieur à 0
-        while (player.hp > 0 && boss.hp > 0) {
-          console.log(` Dans cette étage vous allez affronter ${boss.name}`)
-          //Affichage du nombre des vis des du joueur et de son enemie
-          let answer = affichVie(boss,player)
-          //Verification de l'option que l'utilisateur aura choisie
-          //Si c'est attaque on attaque l'enemie
-          //Sinon on guérit 
-          if (answer === 'Attack') {
-            attack(player, boss)
-          } else {
-            heal(player)
-          }
-          //Réafichage des vies 
-          affichVie(boss,player)
-          //Attaque de l'énemie dans le cas ou ses nombres de vies seront supérieur à 0
-          if(boss.hp > 0){
-            console.log(`${boss.name} attacked and dealt ${boss.str} damage!`);            
-            player.hp -= boss.str; 
-            //Dans le cas ou on auras des vies negatif on attribue la valeur 0 au hp du joueur
-            //C'est à dire qu'il sera égale à 0 et non -6 
-            if (player.hp < 0) {
-              player.hp = 0;
-            }
-          }   
-        }
-        //Verification des vies des utilisateurs
-        //Si ils sont égale à 0 (il a perdu et on sort de la boucle )
-        //Sinon on lui affiche un message de féliciation et le jeux est fini
-        if (player.hp === 0){
-          console.log(`Dommage tu as perdu`)
-          return false   
-        } else  {
-          console.log(`Féliciation!!!!!`)
-          return true
         }
       }
-    }   
+      if (player.hp === 0) {
+        console.log('Dommage tu as perdu');
+        return false;
+      }
+      console.log('Féliciation!!!!!');
+      return true;
+    }
   }
+}
+
 
 export default game;
